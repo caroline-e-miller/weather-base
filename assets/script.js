5ae1ae9ccbca80b25e5d4ad1bd2c013c4516cff5
caroline-e-miller <caroline.miller@ymail.com>
2021-01-29 12:30:28 -0600

    initial push and pseudocode

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// put together CSS/HTML layout
+// look at previous activities for ideas (namely the mini project)
+// link up the weather API
+// create search mechanism (look at mini project)
+// create onclick events for previously searched cities
+// link CSS color to UV values
+// create local storage for previous search values
+// get most recent search value upon refresh of the page
