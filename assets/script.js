e18aa29856bb3fffa76419ff54f71489b4db1fd0
caroline-e-miller <caroline.miller@ymail.com>
2021-02-02 12:57:22 -0600

    added degrees, percent, and UV

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ var latitude = null;
 var longitude = null;
 var city = "";
 var cities = [];
-const dayRange = "&cnt=5&appid=2cef2d7cae052715188e701df4ab1db7";
+const dayRange = "&cnt=5&units=imperial&appid=2cef2d7cae052715188e701df4ab1db7";
 const fahrenheit = "&units=imperial&appid=2cef2d7cae052715188e701df4ab1db7";
 const lonComponent = "&lon="
 const uvAPI = "&appid2cef2d7cae052715188e701df4ab1db7"
@@ -28,6 +28,8 @@ var uvURL = "https://api.openweathermap.org/data/2.5/weather?lat=";
 var forecastURL = "https://api.openweathermap.org/data/2.5/forecast/?q="
 // var forecastURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=";
 var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// var forecastUvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=2cef2d7cae052715188e701df4ab1db7"
 
 
 
@@ -39,17 +41,17 @@ function getCurrentWeather() {
         })
         .then(function (data) {
             currentCity.textContent = data.name;
-            currentTemp.textContent = data.main.temp;
-            currentHumidity.textContent = data.main.humidity;
-            currentWindSpeed.textContent = data.wind.speed;
+            currentTemp.textContent = data.main.temp + "°";
+            currentHumidity.textContent = data.main.humidity + "%";
+            currentWindSpeed.textContent = data.wind.speed + "mph";
             console.log(data);
-            // getUVIndex(data.coord.lat, data.coord.lon);
+            getUVIndex(data.coord.lat, data.coord.lon);
             // get5DayForecast()
         })
 }
 
 function get5DayForecast() {
-    // city = userInput.value;
+    // var forecastUvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=2cef2d7cae052715188e701df4ab1db7"
     fetch(forecastURL + city + dayRange)
         .then(function (response) {
             if (response.ok)
@@ -64,8 +66,8 @@ function get5DayForecast() {
                 var futureTemp = document.createElement("p");
                 var futureHumidity = document.createElement("p");
                 futureDays.setAttribute("class", "col card");
-                futureTemp.textContent = "Temperature: " + data.list[i].main.temp;
-                futureHumidity.textContent = "Humidity: " + data.list[i].main.humidity;
+                futureTemp.textContent = "Temperature: " + data.list[i].main.temp + "°";
+                futureHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
                 futureDays.appendChild(futureTemp);
                 futureDays.appendChild(futureHumidity);
                 forecastArea.appendChild(futureDays);
@@ -75,20 +77,19 @@ function get5DayForecast() {
 }
 
 // put this inside the getCurrentWeather function?
-// function getUVIndex() {
-//     latitude.value = data.coord.lat;
-//     longitude.value = data.coord.lon;
-//     fetch(uvURL + latitude + lonComponent + longitude + uvAPI)
-//         .then(function (response) {
-//             if (response.ok)
-//                 return response.json();
-//         })
-//         .then(function (data) {
-//             currentUV.textContent = 1000
-//             console.log(data);
+function getUVIndex(lat, lon) {
+    var uvURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=2cef2d7cae052715188e701df4ab1db7";
+    fetch(uvURL)
+        .then(function (response) {
+            if (response.ok)
+                return response.json();
+        })
+        .then(function (data) {
+            currentUV.textContent = data[0].value;
+            console.log(data);
 
-//         })
-// }
+        })
+}
 
 function loadSavedCities() {
 
